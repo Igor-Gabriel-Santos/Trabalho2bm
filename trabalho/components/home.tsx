25b5f4c1e7c355b7c0623af9895cb56f500cf22f
Igor-Gabriel-Santos <Gabrieligor022@gmail.com>
2024-06-19 13:27:31 +0000

    save01

--- trabalho/components/home.tsx
+++ trabalho/components/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import Sobre from './sobre';
+
 
 export default function App() {
     const [page, setPage] = React.useState('home');
@@ -8,12 +9,15 @@ export default function App() {
         if (page === 'home') {
             return (
                 <View style={styles.container}>
+                    <View style={styles.quadrado}></View>
                     <Text style={styles.title}>Sensor de localização</Text>
                     <Text style={styles.paragrafo}>aplicativo para mostrar sua </Text>
                     <Text style={styles.paragrafo}>localização do dispositivo</Text>
+                    <Image source={require('../assets/images/images.png')} style={{ width: 300, height: 200, borderRadius: 10,  }} />
                     <TouchableOpacity style={styles.button} onPress={() => setPage('sobre')}>
-                        <Text style={styles.buttonText}>Clique aqui</Text>
+                        <Text style={styles.buttonText}>Localização</Text>
                     </TouchableOpacity>
+                    <View style={styles.quadrado2}></View>
                 </View>
             );
         } else if (page === 'sobre') {
@@ -31,11 +35,25 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#000E3F',
     },
+    quadrado: {
+        width: 900, 
+        height: 70,
+        backgroundColor: '#3657CC',
+        marginTop: -400
+    },
+    quadrado2: {
+        width: 900, 
+        height: 70,
+        backgroundColor: '#3657CC',
+        marginTop: 170,
+        marginBottom: -400,
+    },
     title: {
         fontSize: 35,
         fontWeight: 'bold',
         color: '#ffff',
         marginBottom: 20,
+        marginTop: 20,
     },
     paragrafo: {
         fontSize: 18,
@@ -50,6 +68,8 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 5,
         marginTop: 200,
+        
+        
     },
     buttonText: {
         color: '#FFFFFF',
--- trabalho/components/sobre.tsx
+++ trabalho/components/sobre.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import Home from './home';
 
 
@@ -9,10 +9,15 @@ const renderPage = () => {
     if (page === 'sobre') {
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>Página Sobre</Text>
+                <View style={styles.quadrado}></View>
+                <Text style={styles.title}>Localização</Text>
+                <Text style={styles.paragrafo}>Latitude 0.00 </Text>
+                    <Text style={styles.paragrafo}>longitude 0.00</Text>
+                    <Image source={require('../assets/images/imagess.png')} style={{ width: 300, height: 200, borderRadius: 10, marginTop: 40,  }} />
                 <TouchableOpacity style={styles.button} onPress={() => setPage('home')}>
-                    <Text style={styles.buttonText}>Clique aqui</Text>
+                    <Text style={styles.buttonText}>Obter localização</Text>
                 </TouchableOpacity>
+                <View style={styles.quadrado2}></View>
             </View>
         );
     } else if (page === 'home') {
@@ -21,26 +26,52 @@ const renderPage = () => {
     };
         return <View style={styles.container}>{renderPage()}</View>;
 }
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#fff',
-        },
-        title: {
-            fontSize: 24,
-            fontWeight: 'bold',
-            color: '#000',
-            marginBottom: 20,
-        },
-        button: {
-            backgroundColor: '#007BFF',
-            padding: 10,
-            borderRadius: 5,
-        },
-        buttonText: {
-            color: '#FFFFFF',
-            fontSize: 16,
-        },
-    })
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000E3F',
+    },
+    quadrado: {
+        width: 900, 
+        height: 70,
+        backgroundColor: '#3657CC',
+        marginTop: -400
+    },
+    quadrado2: {
+        width: 900, 
+        height: 70,
+        backgroundColor: '#3657CC',
+        marginTop: 170,
+        marginBottom: -400,
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: '#ffff',
+        marginBottom: 20,
+        marginTop: 20,
+    },
+    paragrafo: {
+        fontSize: 23,
+        fontWeight: 'bold',
+        color: '#ffff',
+        marginBottom: 0,
+        alignItems: 'center',
+        
+    },
+    button: {
+        backgroundColor: '#3657CC',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 200,
+        
+        
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
+
+});
